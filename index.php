209d967612ed5e66f96e98b5684833672a3a48fb
Bruno Daniel da Silva <87128493+Danibruno20@users.noreply.github.com>
2024-04-25 22:51:37 +0000

    mudanças no php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
@@ -8,14 +9,45 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
+<header>
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container">
+      <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Treinos
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Bruno</a></li>
+                <li><a class="dropdown-item" href="#">Caio</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
 
-
-
-
-
-<h1>Segunda-Feira</h1>
-<h3>Treino de Peito e Tríceps</h3>
+  <h1>Segunda-Feira</h1>
+  <h3>Treino de Peito e Tríceps</h3>
 
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
@@ -32,7 +64,20 @@
       </div>
     </div>
   </div>
-
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="media/exercicios/Remada alta com barra.gif" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Remada alta com barra</h5>
+          <p class="card-text"><small class="text-body-secondary">3 séries de 10-12 repetições</small></p>
+          <p class="card-text">A remada alta com barra é um exercício de musculação focado principalmente nos músculos das costas, especialmente nos trapézios, deltoides posteriores e romboides.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 
@@ -48,4 +93,5 @@
     crossorigin="anonymous"></script>
   <script src="script.js"></script>
 </body>
+
 </html>
