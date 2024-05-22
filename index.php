4cf055299cc0131f6e2fd65ba3f351c509cd3a71
Bruno Daniel da Silva <87128493+Danibruno20@users.noreply.github.com>
2024-05-22 21:16:52 +0000

    mudanças

--- index.php
+++ index.php
@@ -6,29 +6,38 @@
   <meta name="viewport" content="width=device-width">
   <title>Treino Bruno</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
 
   <!-- <Header> -->
-    <?php include_once('pages/header.php'); ?>
+  <?php include_once('pages/header.php'); ?>
   <!-- </Header> -->
 
-  
+  <!-- <Main> -->
+  <div class="grid-container">
+    <div class="card-grid">
+      <div class="nome"><h5></h5></div>
+      <div class="gif"><img src="media/exercicios/Crucifixo com halteres.gif" alt=""></div>
+      <div class="descricao"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat illum expedita pariatur cupiditate doloribus nesciunt asperiores, aut obcaecati quidem ipsa provident culpa officiis. Repellat perspiciatis labore dolores totam cupiditate maiores?</p></div>
+      <div class="series"><p></p></div>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  <!-- </Main> -->
 
 
 
 
 
 
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-    crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
   <script src="script.js"></script>
 </body>
 
--- pages/header.php
+++ pages/header.php
@@ -1,30 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Shrikhand&display=swap');
+
+    :root{
+        --cor-principal: #06141b;
+        --segunda-cor: #ccd0cf;
+        --terceira-cor:#253745;
+    }
+    
+    body{
+        font-family: "Shrikhand", serif;
+        margin: 0;
+        padding: 0;
+    }
+    header{
+        width: 100%;
+        height: 30vh;
+        background-color: #06141b;
+        color: #ccd0cf;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
 </style>
 <body>
     <header>
         <div>
-            <div>
-                <a href="index.php">
-                    <img src="../media/site/logo.png">
-                </a>
-            </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </nav>
-            </div>
-            <div></div>
+            <h1>TREINO</h1>
         </div>
     </header>    
 </body>
